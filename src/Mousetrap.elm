27b7067e1e2edc766cc24f26f418667bc875f768
Joe Fiorini <joe@joefiorini.com>
2018-02-05 19:18:28 -0500

    Upgrade to elm 0.18

--- src/Mousetrap.elm
+++ src/Mousetrap.elm
@@ -1,35 +1,48 @@
-module Mousetrap where
+module Mousetrap exposing (..)
+
 {-| This library gives you a Signal that receives a string representing keys that were just pressed. For example, if you hold down Ctrl and press "o" then the Signal will receive the string "ctrl+o". This allows you to map values to keyboard shortcuts in one place, ie. a `case` statement or a set of guards.
+
 
 ## Loading
 
 `import Mousetrap`
 
+
 ## Using
 
 @docs keydown, keyup, keypress
+
 -}
 
 import Signal
 import Native.Mousetrap
 
+
 {-| Tells Mousetrap to fire on keydown events
 
 Returns a Signal that will receive a string representing the keys that were pressed.
+
 -}
 keydown : Signal String
-keydown = Native.Mousetrap.keydown
+keydown =
+    Native.Mousetrap.keydown
+
 
 {-| Tells Mousetrap to fire on keyup events
 
 Returns a Signal that will receive a string representing the keys that were pressed.
+
 -}
 keyup : Signal String
-keyup = Native.Mousetrap.keyup
+keyup =
+    Native.Mousetrap.keyup
+
 
 {-| Tells Mousetrap to fire on keypress events
 
 Returns a Signal that will receive a string representing the keys that were pressed.
+
 -}
 keypress : Signal String
-keypress = Native.Mousetrap.keypress
+keypress =
+    Native.Mousetrap.keypress
